524e0c70683980881bc82bde0c9092b5b302a624
Alex Larson <alex.larson@hotmail.com>
2020-11-15 21:50:50 -0600

    ESP32 support

--- LightOrganClient/src/SocketManager.cpp
+++ LightOrganClient/src/SocketManager.cpp
@@ -1,5 +1,5 @@
 #include "SocketManager.h"
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 
 WiFiClient client;
 
--- LightOrganClient/src/WifiManager.h
+++ LightOrganClient/src/WifiManager.h
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 
 class WifiManager
 {
