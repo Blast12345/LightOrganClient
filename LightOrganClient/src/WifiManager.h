dae9fac447afbdfb55ebbf3c3839a88ead1d41c8
Alex Larson <alex.larson@hotmail.com>
2020-11-22 19:38:13 -0600

    Proof of concept; color appears to be setting without flicker.

--- LightOrganClient/src/WifiManager.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include <WiFi.h>
-
-class WifiManager
-{
-public:
-    static void connect(const char *ssid, const char *password);
-    static boolean isConnected();
-};
--- LightOrganClient/src/main.cpp
+++ LightOrganClient/src/main.cpp
@@ -1,21 +1,12 @@
 // Imports
-#include <Adafruit_NeoPixel.h>
-#include "WifiManager.h"
-#include "SocketManager.h"
-
-// WiFi Configuration
-const char *ssid = "LightOrgan";          // The SSID (name) of the Wi-Fi network you want to connect to
-const char *password = "thecolorofmusic"; // The password of the Wi-Fi network
-const char *serverIP = "192.168.4.1";     // The IP address of the Raspberry Pi //TODO: Improve this comment
-const int port = 9999;                    // The port of the socket we want to connect to
+#include "FastLED.h"
 
 //LED Configuration
-const int LED_COUNT = 300;
+const unsigned int LED_COUNT = 12;
 const int LED_PIN = 13;
 
 //State
-SocketManager socketManager;
-Adafruit_NeoPixel strand = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[LED_COUNT];
 
 // Setup
 void setup()
@@ -25,45 +16,12 @@ void setup()
   delay(10);            // Suggested wait to reduce issues with WiFi. Not sure if needed
 
   // Setup neopixel strand
-  strand.begin();
-  strand.clear();
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
 }
 
 // Loop
 void loop()
 {
-  // If WiFi is disconnected, attempt to connect
-  if (WifiManager::isConnected() == false)
-  {
-    strand.clear();
-    WifiManager::connect(ssid, password);
-    return;
-  }
-
-  // If socket is disconnected, attempt to connect
-  if (socketManager.isConnected() == false)
-  {
-    strand.clear();
-    socketManager.connectToSocket(serverIP, port);
-    socketManager.sendLedCount(LED_COUNT);
-    return;
-  }
-
-  //If connected to the socket, then attempt to retrieve the next command and update the
-  std::vector<RGB> colors = socketManager.getNextCommand();
-
-  //TODO: Create NeopixelManager?
-  for (uint i = 0; i < colors.size(); i++)
-  {
-    RGB color = colors[i];
-
-    int r = color.red;
-    int g = color.green;
-    int b = color.blue;
-
-    strand.setPixelColor(i, strand.Color(r, g, b));
-  }
-
-  Serial.println("Setting pixels...");
-  strand.show();
+  leds[0].setRGB(128, 128, 128);
+  FastLED.show();
 }
