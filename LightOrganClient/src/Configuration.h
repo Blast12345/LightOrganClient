363a40b2eb819a678cc0cc2905554846fd2d00fe
Alex Larson <>
2022-08-18 22:15:51 -0500

    reintroduced config

---
+++ LightOrganClient/src/Configuration.h
@@ -0,0 +1,6 @@
+const char *ssid = "your access point";
+const char *password = "your access point password;
+const char *serverIP = "your server ip";
+const int port = 9999;
+const int ledCount = 300;
+const int ledPin = 13;
