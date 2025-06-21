55249f910d5af26abb02dfd0059904fdd3cd75ee
Blast12345 <germane.clipper.0q@icloud.com>
2025-06-21 12:04:16 -0500

    WIP

--- LightOrganClient/src/main.cpp
+++ LightOrganClient/src/main.cpp
@@ -1,10 +1,25 @@
 #include "Configuration.h"
+#include "helpers/Wait.h"
 #include "networking/LOServer.h"
 #include "networking/Network.h"
-#include "helpers/Wait.h"
+#include <esp_wifi.h>
+
+#include <esp_now.h>
+#include <queue>
 
 Network network(networkCredentials);
 LOServer server(serverPort);
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+  Serial.println("Received");
+
+  // Convert incoming data to a String
+  String receivedData = String((char *)incomingData).substring(0, len);
+
+  // Print the received data
+  Serial.println("Data Received: " + receivedData);
+}
 
 void setup()
 {
@@ -13,37 +28,61 @@ void setup()
 
   Serial.println("Baud rate set to: " + String(baudRate));
 
-  ledChain.setup();
+  // ledChain.setup();
 
   Serial.println("Setup complete.");
+
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  else
+  {
+    Serial.println("ESP-NOW initialized successfully");
+  }
+
+  if (esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv)) != ESP_OK)
+  {
+    Serial.println("Error registering ESP-NOW receive callback");
+  }
+  else
+  {
+    Serial.println("ESP-NOW receive callback registered successfully");
+  }
 }
 
 // cppcheck-suppress unusedFunction
 void loop()
 {
-  if (network.isDisconnected())
-  {
-    network.connect();
-    waitOneSecond(); // Give the device time to be assigned an IP and whatnot.
-    network.printConnectionInformation();
-  }
+  // waitOneSecond(); // Give the device some time to warm up or weird things tend to happen.
+  // Serial.println("Looping...");
+  // if (network.isDisconnected())
+  // {
+  //   network.connect();
+  //   waitOneSecond(); // Give the device time to be assigned an IP and whatnot.
+  //   network.printConnectionInformation();
+  // }
 
-  if (network.isConnected() && server.isNotListening())
-  {
-    try
-    {
-      server.beginListening();
-    }
-    catch (const std::runtime_error &e)
-    {
-      Serial.println("Error: " + String(e.what()));
-      waitOneSecond();
-    }
-  }
+  // if (network.isConnected() && server.isNotListening())
+  // {
+  //   try
+  //   {
+  //     server.beginListening();
+  //   }
+  //   catch (const std::runtime_error &e)
+  //   {
+  //     Serial.println("Error: " + String(e.what()));
+  //     waitOneSecond();
+  //   }
+  // }
 
-  if (network.isConnected() && server.isListening())
-  {
-    server.onNextColor([](Color color)
-                       { ledChain.setAllTo(color); });
-  }
+  // if (network.isConnected() && server.isListening())
+  // {
+  //   server.onNextColor([](Color color)
+  //                      { ledChain.setAllTo(color); });
+  // }
 }
